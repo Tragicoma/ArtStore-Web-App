026424c25882638f267b9d7e039c21a163e6328f
Madara Priede <priede.madara@gmail.com>
2021-07-05 22:14:26 +0300

    Added base controllers and views - clean project.

--- ArtStore.Ui/Controllers/HomeController.cs
+++ ArtStore.Ui/Controllers/HomeController.cs
@@ -23,10 +23,12 @@ namespace ArtStore.Ui.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
