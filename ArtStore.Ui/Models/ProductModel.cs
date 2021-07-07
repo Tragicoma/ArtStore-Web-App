2e557054db2b7ce47c838972ba696d1af048acce
Madara Priede <priede.madara@gmail.com>
2021-07-07 22:21:18 +0300

    Cleaned up Contact a bit.

---
+++ ArtStore.Ui/Models/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArtStore.Ui.Models
+{
+    public class ProductModel
+    {
+        int ID { get; set; }
+        string Name { get; set; }
+        double Pride { get; set; }
+        paintingAuthor Author { get; set; }
+
+    }
+}
---
+++ ArtStore.Ui/Models/paintingAuthor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArtStore.Ui.Models
+{
+    public class paintingAuthor
+    {
+        int Id { get; set; }
+        string FullName { get; set; }
+        string Country { get; set; }
+        string Portfolio { get; set; } // link to somewhere?
+    }
+}
