898a470496e9414087aa4a488fc2b57455f20a2c
Anna Bu <annabusele@inbox.lv>
2021-07-13 18:34:28 +0300

    Fixed filter

--- ArtStore.Ui/wwwroot/js/site.js
+++ ArtStore.Ui/wwwroot/js/site.js
@@ -2,21 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-$(document).ready(function () {
-    $('.category_item').click(function () {
-        var category = $(this).attr('id');
+//$(document).ready(function () {
+//    $('.category_item').click(function () {
+//        var category = $(this).attr('id');
 
-        if (category == 'all') {
-            $('.art_item').addClass('hide');
-            setTimeout(function () {
-                $('.art_item').removeClass('hide');
-            }, 300);
+//        if (category == 'all') {
+//            $('.art_item').addClass('hide');
+//            setTimeout(function () {
+//                $('.art_item').removeClass('hide');
+//            }, 300);
 
-        } else {
-            $('.art_item').addClass('hide');
-            setTimeout(function () {
-                $('. + category'.removeClass('hide'));
-            }, 300);
-        }
-    });
-});
+//        } else {
+//            $('.art_item').addClass('hide');
+//            setTimeout(function () {
+//                $('. + category'.removeClass('hide'));
+//            }, 300);
+//        }
+//    });
+//});
